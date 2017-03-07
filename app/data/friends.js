5db02fb8add97e0279927b4a514170717e51716e
Melissa Munson <melissamunson@guest-2-00702.dhcp.unc.edu>
2017-03-07 18:27:12 -0500

    almost final

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,23 @@ var friendArray = [
 		name: "Melissa",
 		photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rough_Coated_Jack_Russell_Terrier.JPG/220px-Rough_Coated_Jack_Russell_Terrier.JPG",
 		scores: [3,3,3,3,3,3,3,3,3,3]
+	},
+	{
+		name: "Evie",
+		photo: "https://www.facebook.com/photo.php?fbid=10152788138043154&set=pcb.10152788142173154&type=3&theater",
+		scores: [5,5,5,1,5,2,1,3,3,1]
+	}
+	,
+	{
+		name: "MM",
+		photo: "https://www.facebook.com/photo.php?fbid=10154561871853154&set=t.587104054&type=3&theater",
+		scores: [4,4,4,4,4,4,4,4,4,4]
+	}
+	,
+	{
+		name: "MM",
+		photo: "https://www.facebook.com/photo.php?fbid=10154561871853154&set=t.587104054&type=3&theater",
+		scores: [4,4,4,4,4,4,4,4,4,4]
 	}
 ];
 
